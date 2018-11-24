4459072979de071eb269227c5e0e0939c12874b0
Benjamin Viramontes <benja120599@gmail.com>
2018-11-23 22:46:19 -0600

    Se agrego el metodo ordenamientoRadixSort

--- EjemplosMetodosOrdenamiento.py
+++ EjemplosMetodosOrdenamiento.py
@@ -152,60 +152,67 @@ class MetodosOrdenamiento:
         m.mostrarDatosEficiencia(None, contRecorrido5, contIntercambios5, contComparaciones5, tiempoFinal)
     
     def ordenamientoShellSort(self, vector):
-        '''
+        
         m = MetodosOrdenamiento
         
         contRecorrido6 = 0
         contComparaciones6 = 0
         contIntercambios6 = 0
         
-        salto=0 
-        aux=0 
-        i=0
-        cambios = True
         
-        for i in range(salto = len(vector/2), (salto=numeros.length/2; salto != 0; salto/=2) {
-            cambios = true;
-            while(cambios) {
-                cambios = false;
-                for(i = salto; i<numeros.length; i++) {
-                    contComparaciones6++;
-                    if(numeros[i-salto] > numeros[i]) {
-                        aux = numeros[i];
-                        numeros[i] = numeros[i-salto];
-                        numeros[i-salto] = aux;
-                        cambios = true;
-                        contIntercambios6++;
-                    }
-                    contRecorrido6++;
-                }
-            }
-        }
+        n =  len(vector)
+        brecha = n/2
+    
+        while (brecha >  0):
+            for i in range (int(brecha), n):
+                val = vector[i]
+                j = i
+                contComparaciones6+=1
+    
+                while( (j >= int(brecha)) and (vector[j-int(brecha)] > val) ):
+                    vector[j] = vector[j - int(brecha)]
+                    j -= int(brecha)
+                    contRecorrido6 += 1
+    
+                vector[j] = val
+                contIntercambios6+=1
+    
+            brecha /=  2
         
-        mostrarVector(numeros);
-        tiempoFinal = System.nanoTime() - tiempoInicial;
+        m.mostrarVector(None, vector)
+        tiempoFinal = timeit.timeit()
+        m.mostrarDatosEficiencia(None, contRecorrido6, contIntercambios6, contComparaciones6, tiempoFinal)
+    
+    def particion(self, lista, izq, der):
         
-        mostrarDatosEficiencia(contRecorrido6, contComparaciones6, contIntercambios6, tiempoFinal);
-        '''
+        pivote = lista[der]
+        indice = izq
+    
+        for i in range(izq, der):
+            
+            if lista[i] <= pivote:
+                lista[indice], lista[i] = lista[i], lista[indice]
+                indice += 1
+    
+        lista[indice], lista[der] = lista[der], lista[indice]
+        return indice
+        
+    def ordenamientoQuickSort(self, vector, izq, der):
+        
+        m = MetodosOrdenamiento
+        
+        contRecorrido7=0
+        contComparaciones7=0
+        contIntercambios7=0
+        
+        if izq < der:
+            pivote_indice = m.particion(None, vector, izq, der)
+            m.ordenamientoQuickSort(None, vector, izq, pivote_indice-1)
+            m.ordenamientoQuickSort(None, vector, pivote_indice+1, der)
+        
+        tiempoFinal = timeit.timeit()
+        return vector
 
-    def ordenamientoQuickSort(self, vector):
-        
-        izquierda=[]
-        centro=[]
-        derecha=[]
-        if (len(vector)>1):
-            pivote = vector[0]
-            for i in vector:
-                if i<pivote:
-                    izquierda.append(i)
-                elif i==pivote:
-                    centro.append(i)
-                elif i>pivote:
-                    derecha.append(i)
-        
-            return mo.ordenamientoQuickSort(None, izquierda) + centro + mo.ordenamientoQuickSort(None, derecha)
-        else:
-            return vector
     
     def mostrarVector(self, vector):
         
@@ -273,8 +280,14 @@ mo = MetodosOrdenamiento
 
 lista = [6, 4, 1, 10, 5, 9, 7, 2, 8, 3]
 
-mo.mostrarVector(None, mo.vector1000(None).copy())
+#mo.mostrarVector(None, mo.vector1000(None).copy())
 #mo.mostrarVector(None, lista)
+
+arreglo1 = mo.vector1000(None)
+arreglo2 = mo.vector10000(None)
+arreglo3 = mo.vector100000(None)
+arreglo4 = mo.vector1000000(None)
+
 
 opcion = 0
 
@@ -285,7 +298,9 @@ while(opcion != 10):
     print("1) Ordenamiento Burbuja.")
     print("2) Ordenamiento Por Seleccion.")
     print("3) Ordenamiento Por Insercion.")
-    print("4) Ordenamiento ShellSort")
+    print("4) Ordenamiento ShellSort.")
+    print("5) Ordenamiento QuickSort.")
+    print("6) Ordenamiento RadixSort. ")
     print("10) Salir.")
     print()
     opcion = int(raw_input("Elija una opcion: "))
@@ -295,10 +310,10 @@ while(opcion != 10):
         print()
         print("=========== ORDENAMIENTO CON BURBUJA 1 ===========")
         #mo.ordenamientoBurbuja1(None, lista)
-        mo.ordenamientoBurbuja1(None, mo.vector1000(None).copy())
-        mo.ordenamientoBurbuja1(None, mo.vector10000(None).copy())
-        mo.ordenamientoBurbuja1(None, mo.vector100000(None).copy())
-        mo.ordenamientoBurbuja1(None, mo.vector1000000().copy())
+        mo.ordenamientoBurbuja1(None, arreglo1.copy())
+        mo.ordenamientoBurbuja1(None, arreglo2.copy())
+        mo.ordenamientoBurbuja1(None, arreglo3.copy())
+        mo.ordenamientoBurbuja1(None, arreglo4.copy())
         #mo.ordenamientoBurbuja1(None, lista)
                     
         print()
@@ -306,10 +321,10 @@ while(opcion != 10):
         print()
                  
         print("=========== ORDENAMIENTO CON BURBUJA 2 ===========")
-        mo.ordenamientoBurbuja2(None, mo.vector1000(None).copy())
-        mo.ordenamientoBurbuja2(None, mo.vector10000(None).copy())
-        mo.ordenamientoBurbuja2(None, mo.vector100000(None).copy())
-        mo.ordenamientoBurbuja2(None, mo.vector1000000(None).copy())
+        mo.ordenamientoBurbuja2(None, arreglo1.copy())
+        mo.ordenamientoBurbuja2(None, arreglo2.copy())
+        mo.ordenamientoBurbuja2(None, arreglo3.copy())
+        mo.ordenamientoBurbuja2(None, arreglo4.copy())
         #mo.ordenamientoBurbuja2(None, lista)
                 
         print()
@@ -317,10 +332,10 @@ while(opcion != 10):
         print()
                   
         print("=========== ORDENAMIENTO CON BURBUJA 3 ===========")   
-        mo.ordenamientoBurbuja3(None, mo.vector1000(None).copy())
-        mo.ordenamientoBurbuja3(None, mo.vector10000(None).copy())
-        mo.ordenamientoBurbuja3(None, mo.vector100000(None).copy())
-        mo.ordenamientoBurbuja3(None, mo.vector1000000(None).copy())
+        mo.ordenamientoBurbuja3(None, arreglo1.copy())
+        mo.ordenamientoBurbuja3(None, arreglo2.copy())
+        mo.ordenamientoBurbuja3(None, arreglo3.copy())
+        mo.ordenamientoBurbuja3(None, arreglo4.copy())
         #mo.ordenamientoBurbuja3(None, lista)
                 
         print()
@@ -333,10 +348,10 @@ while(opcion != 10):
         print()
                     
         print("=========== ORDENAMIENTO POR SELECCION ===========")  
-        mo.ordenamientoPorSeleccion(None, mo.vector1000(None).copy())
-        mo.ordenamientoPorSeleccion(None, mo.vector10000(None).copy())
-        mo.ordenamientoPorSeleccion(None, mo.vector100000(None).copy())
-        mo.ordenamientoPorSeleccion(None, mo.vector1000000(None).copy())
+        mo.ordenamientoPorSeleccion(None, arreglo1.copy())
+        mo.ordenamientoPorSeleccion(None, arreglo2.copy())
+        mo.ordenamientoPorSeleccion(None, arreglo3.copy())
+        mo.ordenamientoPorSeleccion(None, arreglo4.copy())
         #mo.ordenamientoPorSeleccion(None, lista)
         
         print()
@@ -350,10 +365,10 @@ while(opcion != 10):
         print()
                 
         print("=========== ORDENAMIENTO POR INSERCION ===========")
-        mo.ordenamientoPorInsercion(None, mo.vector1000(None).copy())
-        mo.ordenamientoPorInsercion(None, mo.vector10000(None).copy())
-        mo.ordenamientoPorinsercion(None, mo.vector100000(None).copy())
-        mo.ordenamientoPorInsercion(None, mo.vector1000000(None).copy())
+        mo.ordenamientoPorInsercion(None, arreglo1.copy())
+        mo.ordenamientoPorInsercion(None, arreglo2.copy())
+        mo.ordenamientoPorinsercion(None, arreglo3.copy())
+        mo.ordenamientoPorInsercion(None, arreglo4.copy())
         #mo.ordenamientoPorInsercion(None, lista)
         
         print()
@@ -366,11 +381,48 @@ while(opcion != 10):
         print()
                 
         print("=========== ORDENAMIENTO SHELLSORT ===========")
-        
+        mo.ordenamientoShellSort(None, arreglo1.copy())
+        mo.ordenamientoShellSort(None, arreglo2.copy())
+        mo.ordenamientoShellSort(None, arreglo3.copy())
+        mo.ordenamientoShellSort(None, arreglo4.copy())
         
         print()
         print("---------------------------------------------------------------------------------")
         print()         
+    
+    elif(opcion == 5):
+        print()
+        print("---------------------------------------------------------------------------------")
+        print()
+                
+        print("=========== ORDENAMIENTO QUICKSORT ===========")
+        print(mo.ordenamientoQuickSort(None, mo.vector1000(None).copy(), 0, len(mo.vector1000(None).copy())-1))
+        #mo.ordenamientoQuickSort(None, mo.vector10000(None).copy())
+        #mo.ordenamientoQuickSort(None, mo.vector100000(None).copy())
+        #mo.ordenamientoQuickSort(None, mo.vector1000000(None).copy())
+        #mo.ordenamientoQuickSort(None, lista.copy())
+        
+        
+        print()
+        print("---------------------------------------------------------------------------------")
+        print()
+   
+    elif(opcion == 6):
+        print()
+        print("---------------------------------------------------------------------------------")
+        print()
+                
+        print("=========== ORDENAMIENTO RADIXSORT ===========")
+        #mo.ordenamientoRadixSort(None, mo.vector1000(None).copy())
+        #mo.ordenamientoRadixSort(None, mo.vector10000(None).copy())
+        #mo.ordenamientoRadixSort(None, mo.vector100000(None).copy())
+        #mo.ordenamientoRadixSort(None, mo.vector1000000(None).copy())
+        mo.ordenamientoRadixSort(None, lista.copy())
+        
+        
+        print()
+        print("---------------------------------------------------------------------------------")
+        print()                  
     
     elif(opcion == 0):
         print("S A L I E N D O . . .")
